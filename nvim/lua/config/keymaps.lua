47380a05b993f8d8a8dbff0d24016a5921216203
Bernat Skrabec <bernatgene@gmail.com>
2025-08-24 15:40:35 +0200

    fzf-lua; though still using telescope for obsidian.nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,35 @@ end, { desc = "Git graph (flog)" })
 
 map("v", "<leader>y", '"+y', { desc = "Copy to system clipboard" })
 map("v", "<leader>x", '"+x', { desc = "Cut to system clipboard" })
+
+--spell
+vim.keymap.set("n", "<leader>r", function()
+  local word = vim.fn.expand("<cword>")
+  local suggestions = vim.fn.spellsuggest(word)
+  if #suggestions == 0 then
+    vim.notify("No spelling suggestions")
+    return
+  end
+  vim.ui.select(suggestions, { prompt = "Spelling suggestions:" }, function(choice)
+    if choice then
+      vim.cmd("normal! ciw" .. choice)
+    end
+  end)
+end, { desc = "Spell suggest" })
+
+vim.keymap.set("n", "<leader>z", function()
+  local langs = { "en", "es", "ca" }
+  local current = vim.opt.spelllang:get()[1] or "none"
+  vim.ui.select(langs, {
+    prompt = "Select spell language (current: " .. current .. ")",
+    format_item = function(item)
+      return item == current and (item .. " ✓") or item
+    end,
+  }, function(choice)
+    if choice then
+      vim.opt.spell = true
+      vim.opt.spelllang = { choice }
+      vim.notify("Spell language set to " .. choice, vim.log.levels.INFO)
+    end
+  end)
+end, { desc = "Set spell language" })
