f2c4ebcefe3b0551eb7af06cbc25f7e40317aeed
Bernat Skrabec <bernatgene@gmail.com>
2025-10-16 21:20:32 +0200

    set ruff as python formatter

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -2,6 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
     opts.formatters_by_ft.astro = { "prettier" }
+    opts.formatters_by_ft.python = { "ruff_format" }
   end,
   keys = {
     {
