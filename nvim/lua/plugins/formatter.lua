1b2344fb7941fa68dd08ab6f36c3a57a29b70caf
Bernat <bernat.gene@spascat.com>
2026-04-20 17:38:12 +0200

    use prettier or biome according to repo config

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,8 +1,31 @@
 return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
-    opts.formatters_by_ft.astro = { "prettier" }
-    opts.formatters_by_ft.svelte = { "prettier" }
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters = opts.formatters or {}
+
+    local biome_configs = { "biome.json", "biome.jsonc" }
+
+    opts.formatters.biome = {
+      condition = function(_, ctx)
+        return vim.fs.find(biome_configs, { path = ctx.filename, upward = true })[1] ~= nil
+      end,
+    }
+
+    local biome_or_prettier = { "biome", "prettier", stop_after_first = true }
+    local filetypes = {
+      "astro",
+      "svelte",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    }
+
+    for _, ft in ipairs(filetypes) do
+      opts.formatters_by_ft[ft] = biome_or_prettier
+    end
+
     opts.formatters_by_ft.python = { "ruff_format" }
   end,
   keys = {
